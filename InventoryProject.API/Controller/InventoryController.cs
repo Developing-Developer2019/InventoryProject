3cbbbd5e3de4579550c35fb85b2bfd75d26a5de3
Marvin Brereton <56225640+Developing-Developer2019@users.noreply.github.com>
2025-04-15 20:47:48 +0100

    Added base endpoints to InventoryController

--- InventoryProject.API/Controller/InventoryController.cs
+++ InventoryProject.API/Controller/InventoryController.cs
@@ -1,8 +1,39 @@
+using InventoryProject.Core.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InventoryProject.API.Controller;
 
+[ApiController]
+[Route("api/[controller]")]
 public class InventoryController : ControllerBase
 {
-    
+    [HttpGet]
+    public async Task<IActionResult> GetAllItems()
+    {
+        return Ok(new List<Item>());
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetItemById(Guid id)
+    {
+        return Ok(new Item());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateItem(Item item)
+    {
+        return Ok("Item created");
+    }
+
+    [HttpPatch]
+    public async Task<IActionResult> UpdateItem(Item item)
+    {
+        return Ok("Item updated");
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteItem(Guid id)
+    {
+        return Ok("Item deleted");
+    }
 }
