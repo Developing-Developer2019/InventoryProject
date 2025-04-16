0280fbd37e6a2f61bfb385d3e819984c8fdcffed
Marvin Brereton <56225640+Developing-Developer2019@users.noreply.github.com>
2025-04-16 11:47:58 +0100

    Added Enum and Constants for use in ItemResponse

---
+++ InventoryProject.Core/Constants/StringConstants.cs
@@ -0,0 +1,7 @@
+namespace InventoryProject.Core.Constants;
+
+public static class StringConstants
+{
+    public static string InStock => "In Stock";
+    public static string SoldOut => "Sold Out";
+}
---
+++ InventoryProject.Core/Enum/DiscountAmount.cs
@@ -0,0 +1,9 @@
+namespace InventoryProject.Core.Enum;
+
+public enum DiscountAmount
+{
+    None = 0,
+    Ten = 10,
+    Twenty = 20,
+    Fifty = 50
+}
