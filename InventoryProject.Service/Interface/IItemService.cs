7a6078880f71d93fb91f1371c23cd3063c835c80
Marvin Brereton <56225640+Developing-Developer2019@users.noreply.github.com>
2025-04-16 11:47:05 +0100

    Added ItemService method to get all items

--- InventoryProject.Service/Interface/IItemService.cs
+++ InventoryProject.Service/Interface/IItemService.cs
@@ -1,6 +1,8 @@
+using InventoryProject.Core.Model.API;
+
 namespace InventoryProject.Service.Interface;
 
 public interface IItemService
 {
-    
+    Task<List<ItemResponse>> GetAllItemsAsync();
 }
